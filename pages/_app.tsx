f1d01de8514f717a86759b00dd6df4c85a6925cd
Dallas James <dallastjames@gmail.com>
2022-10-19 15:43:20 -0500

    add supabase type defs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,28 @@
+import { Database } from "@/utils/db-definitions";
 import { CssVarsProvider } from "@mui/joy/styles";
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { Session, SessionContextProvider } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
-import { useState } from 'react';
+import { useState } from "react";
 
-function MyApp({ Component, pageProps }: AppProps<{ initialSession: Session | null }>) {
-	const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session | null }>) {
+  const [supabaseClient] = useState(() =>
+    createBrowserSupabaseClient<Database>()
+  );
 
-	return (
-		<SessionContextProvider
-			supabaseClient={supabaseClient}
-			initialSession={pageProps.initialSession}
-		>
-			<CssVarsProvider>
-				<Component {...pageProps} />
-			</CssVarsProvider>
-		</SessionContextProvider>
-	);
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <CssVarsProvider>
+        <Component {...pageProps} />
+      </CssVarsProvider>
+    </SessionContextProvider>
+  );
 }
 
 export default MyApp;
---
+++ utils/db-definitions.ts
@@ -0,0 +1,109 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[];
+
+export interface Database {
+  public: {
+    Tables: {
+      package: {
+        Row: {
+          package_id: string;
+          name: string;
+          description: string | null;
+          user_id: string;
+          keywords: string[];
+          fts: unknown | null;
+        };
+        Insert: {
+          package_id: string;
+          name: string;
+          description?: string | null;
+          user_id?: string;
+          keywords?: string[];
+          fts?: unknown | null;
+        };
+        Update: {
+          package_id?: string;
+          name?: string;
+          description?: string | null;
+          user_id?: string;
+          keywords?: string[];
+          fts?: unknown | null;
+        };
+      };
+      package_details: {
+        Row: {
+          package_id: string;
+          last_updated: string;
+          like_count: number;
+          rating_count: number;
+          rating_sum: number;
+        };
+        Insert: {
+          package_id: string;
+          last_updated?: string;
+          like_count?: number;
+          rating_count?: number;
+          rating_sum?: number;
+        };
+        Update: {
+          package_id?: string;
+          last_updated?: string;
+          like_count?: number;
+          rating_count?: number;
+          rating_sum?: number;
+        };
+      };
+      package_like: {
+        Row: {
+          package_id: string;
+          user_id: string;
+          date: string;
+        };
+        Insert: {
+          package_id: string;
+          user_id: string;
+          date?: string;
+        };
+        Update: {
+          package_id?: string;
+          user_id?: string;
+          date?: string;
+        };
+      };
+      package_rating: {
+        Row: {
+          package_id: string;
+          user_id: string;
+          rating: number;
+          last_updated: string;
+        };
+        Insert: {
+          package_id: string;
+          user_id: string;
+          rating?: number;
+          last_updated?: string;
+        };
+        Update: {
+          package_id?: string;
+          user_id?: string;
+          rating?: number;
+          last_updated?: string;
+        };
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+  };
+}
