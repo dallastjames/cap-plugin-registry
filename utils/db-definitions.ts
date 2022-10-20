4ca8cf49a64561a1a695b06b2763976e04901388
Dallas James <dallastjames@gmail.com>
2022-10-20 09:52:55 -0500

    update db definitions

--- utils/db-definitions.ts
+++ utils/db-definitions.ts
@@ -4,106 +4,118 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       package: {
         Row: {
-          package_id: string;
-          name: string;
-          description: string | null;
-          user_id: string;
-          keywords: string[];
-          fts: unknown | null;
-        };
+          package_id: string
+          name: string
+          description: string | null
+          category: Database["public"]["Enums"]["package_category"]
+          user_id: string
+          keywords: string[]
+          fts: unknown | null
+        }
         Insert: {
-          package_id: string;
-          name: string;
-          description?: string | null;
-          user_id?: string;
-          keywords?: string[];
-          fts?: unknown | null;
-        };
+          package_id: string
+          name: string
+          description?: string | null
+          category: Database["public"]["Enums"]["package_category"]
+          user_id?: string
+          keywords?: string[]
+          fts?: unknown | null
+        }
         Update: {
-          package_id?: string;
-          name?: string;
-          description?: string | null;
-          user_id?: string;
-          keywords?: string[];
-          fts?: unknown | null;
-        };
-      };
+          package_id?: string
+          name?: string
+          description?: string | null
+          category?: Database["public"]["Enums"]["package_category"]
+          user_id?: string
+          keywords?: string[]
+          fts?: unknown | null
+        }
+      }
       package_details: {
         Row: {
-          package_id: string;
-          last_updated: string;
-          like_count: number;
-          rating_count: number;
-          rating_sum: number;
-        };
+          package_id: string
+          last_updated: string
+          like_count: number
+          rating_count: number
+          rating_sum: number
+        }
         Insert: {
-          package_id: string;
-          last_updated?: string;
-          like_count?: number;
-          rating_count?: number;
-          rating_sum?: number;
-        };
+          package_id: string
+          last_updated?: string
+          like_count?: number
+          rating_count?: number
+          rating_sum?: number
+        }
         Update: {
-          package_id?: string;
-          last_updated?: string;
-          like_count?: number;
-          rating_count?: number;
-          rating_sum?: number;
-        };
-      };
+          package_id?: string
+          last_updated?: string
+          like_count?: number
+          rating_count?: number
+          rating_sum?: number
+        }
+      }
       package_like: {
         Row: {
-          package_id: string;
-          user_id: string;
-          date: string;
-        };
+          package_id: string
+          user_id: string
+          date: string
+        }
         Insert: {
-          package_id: string;
-          user_id: string;
-          date?: string;
-        };
+          package_id: string
+          user_id: string
+          date?: string
+        }
         Update: {
-          package_id?: string;
-          user_id?: string;
-          date?: string;
-        };
-      };
+          package_id?: string
+          user_id?: string
+          date?: string
+        }
+      }
       package_rating: {
         Row: {
-          package_id: string;
-          user_id: string;
-          rating: number;
-          last_updated: string;
-        };
+          package_id: string
+          user_id: string
+          rating: number
+          last_updated: string
+        }
         Insert: {
-          package_id: string;
-          user_id: string;
-          rating?: number;
-          last_updated?: string;
-        };
+          package_id: string
+          user_id: string
+          rating?: number
+          last_updated?: string
+        }
         Update: {
-          package_id?: string;
-          user_id?: string;
-          rating?: number;
-          last_updated?: string;
-        };
-      };
-    };
+          package_id?: string
+          user_id?: string
+          rating?: number
+          last_updated?: string
+        }
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
-  };
+      package_category:
+        | "authentication"
+        | "storage"
+        | "hardware"
+        | "communication"
+        | "sdkintegration"
+        | "analytics"
+        | "platform"
+        | "behavior"
+        | "security"
+    }
+  }
 }
