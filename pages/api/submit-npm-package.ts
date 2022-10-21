d8c0d077466aaa8b132aa2cdcbb715aa205bbd61
Dallas James <dallastjames@gmail.com>
2022-10-21 10:57:08 -0500

    submit flow

---
+++ pages/api/submit-npm-package.ts
@@ -0,0 +1,41 @@
+import {
+  BAD_REQUEST,
+  METHOD_NOT_ALLOWED,
+  NOT_FOUND,
+  SUCCESS,
+} from "@/utils/http-codes";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.status(METHOD_NOT_ALLOWED).send({ error: "Method Not Allowed" });
+    return;
+  }
+  const packageId = req.query?.packageId ?? null;
+  if (!packageId) {
+    res
+      .status(BAD_REQUEST)
+      .send({ error: "Package ID query param is required" });
+    return;
+  }
+  const npmRes = await fetch(`https://registry.npmjs.com/${packageId}/latest`, {
+    method: "GET",
+  });
+  const packageDetails = await npmRes.json();
+  if (typeof packageDetails === "string") {
+    res.status(NOT_FOUND).send({ error: "Package Not Found" });
+    return;
+  }
+  if (
+    !packageDetails?.devDependencies?.["@capacitor/core"] &&
+    !packageDetails?.peerDependencies?.["@capacitor/core"] &&
+    !packageDetails?.dependencies?.["@capacitor/core"]
+  ) {
+    res.status(BAD_REQUEST).send({ error: "Not a Capacitor Plugin" });
+    return;
+  }
+  res.status(SUCCESS).json(packageDetails);
+}
